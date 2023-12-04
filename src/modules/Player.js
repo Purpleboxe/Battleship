2199cd006b0e2774a08d574700424c51e6bd1e48
Antonio Zamora <Antoniozamora21331@gmail.com>
2023-12-03 19:46:58 -0800

    Add Player.js

---
+++ src/modules/Player.js
@@ -0,0 +1,32 @@
+const Gameboard = require('./Gameboard');
+
+class Player {
+    constructor () {
+        this.gameboard = new Gameboard();
+    }
+
+    attack (enemyGameboard, row, col) {
+        return enemyGameboard.receiveAttack(row, col);
+    }
+}
+
+class Computer extends Player {
+    constructor () {
+        this.previousAttacks = [];
+    }
+
+    makeRandomAttack (enemyGameboard) {
+        let row, col;
+
+        do {
+            row = Math.floor(Math.random() * 10);
+            col = Math.floor(Math.random() * 10);
+        } while (this.previousAttacks.some(attack => attack.row === row && attack.col === col));
+
+        this.previousAttacks.push({row, col});
+
+        return this.attack(enemyGameboard, row, col);
+    }
+}
+
+module.exports = {Player, Computer};
