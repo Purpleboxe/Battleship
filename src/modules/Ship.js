7ee03ae802c017c83281729d3518d9c26f343a66
Antonio Zamora <Antoniozamora21331@gmail.com>
2023-12-15 21:16:57 -0800

    Add new placeShipsRandomly function

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -1,37 +1,35 @@
 class Ship {
-    constructor (length, isVertical) {
-        this.length = length;
-        this.isVertical = isVertical;
-        this.hits = Array(length).fill(false);
-        this.row = null;
-        this.col = null;
-    }
+  constructor(length, isVertical) {
+    this.length = length;
+    this.isVertical = isVertical;
+    this.hits = Array(length).fill(false);
+    this.row = null;
+    this.col = null;
+  }
 
-    setPosition (row, col) {
-        this.row = row;
-        this.col = col;
-    }
+  setPosition(row, col) {
+    this.row = row;
+    this.col = col;
+  }
 
-    hit (square) {
-        if (!this.hits[square]) {
-            this.hits[square] = true;
-            return true;
-        } else {
-            return false;
-        }
+  hit(square) {
+    if (!this.hits[square]) {
+      this.hits[square] = true;
+      return true;
     }
+    return false;
+  }
 
-    isHit (x, y) {
-        if (this.isVertical) {
-            return this.hits[x - this.row];
-        } else {
-            return this.hits[y - this.col];
-        }
+  isHit(x, y) {
+    if (this.isVertical) {
+      return this.hits[x - this.row];
     }
+    return this.hits[y - this.col];
+  }
 
-    isSunk () {
-        return this.hits.every(hit => hit);
-    }
+  isSunk() {
+    return this.hits.every((hit) => hit);
+  }
 }
 
 module.exports = Ship;
